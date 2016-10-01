5c57be11bbe6ab22cec82920fb2568fdef719d20
Matt Parker <moonmaster9000@gmail.com>
2016-10-01 09:33:10 -0400

    First Use Case Sketched; now decide on first unit test

---
+++ CashRegister/src/main/java/cashregister/domain/usecases/CreateItem.java
@@ -0,0 +1,7 @@
+package cashregister.domain.usecases;
+
+public class CreateItem {
+    public static void createItem(String displayName, String barcode, int priceInCents) {
+
+    }
+}
---
+++ CashRegister/src/test/java/cashregister/stepdefs/PurchaseSingleItem.java
@@ -0,0 +1,26 @@
+package cashregister.stepdefs;
+
+import cashregister.domain.usecases.CreateItem;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class PurchaseSingleItem {
+    @Given("^an admin has added \"([^\"]*)\" brand milk to the cash register system, including display name, barcode, and price in cents$")
+    public void anAdminHasAddedBrandMilkToTheCashRegisterSystemIncludingDisplayNameBarcodeAndPriceInCents(String displayName) throws Throwable {
+        CreateItem.createItem(displayName, "barcode", 1);
+    }
+
+    @When("^a cashier scans in the barcode of \"([^\"]*)\"$")
+    public void aCashierScansInTheBarcodeOf(String displayName) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the register should display a total equal to the price of the \"([^\"]*)\"$")
+    public void theRegisterShouldDisplayATotalEqualToThePriceOfThe(String displayName) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
