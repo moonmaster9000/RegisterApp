2bded69ef511b19326ec7aca510eadc3b51d72c8
Matt Parker <moonmaster9000@gmail.com>
2016-10-12 22:48:55 -0400

    hook up createItem usecase in API

---
+++ api/src/main/java/webFrontend/App.java
@@ -0,0 +1,26 @@
+package webFrontend;
+
+import cashregister.domain.repositories.interfaces.ItemRepository;
+import hibernateBackedRepos.repositories.HibernateItemRepo;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public ItemRepository itemRepository(){
+        SessionFactory sessionFactory;
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        return new HibernateItemRepo(sessionFactory.openSession());
+    }
+}
